4c7282c9805d7ea94a2f7348644b3c27f338562b
Ahmed <ahmedshimy64@gmail.com>
2024-06-02 03:53:43 +0300

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        hello world
+      </h1>
+      <form className={styles.input}>
+      <label for="fname">First name:</label>
+<input type="text" id="fname" name="fname">
+
+</input>
+      
+
+        
+      </form>
     </div>
   );
 }
